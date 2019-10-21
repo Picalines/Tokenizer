a93ed284fbcf6b2301927ef461f0aaf1298bc0d9
Пётр Костенко <picalines0@gmail.com>
2019-10-21 22:41:37 +0300

    Добавьте файлы проекта.

---
+++ Tokenizer/Token.cs
@@ -0,0 +1,37 @@
+﻿namespace PicTokenizer
+{
+    public interface IToken<T>
+    {
+        T type { get; }
+        string value { get; }
+        int position { get; }
+    }
+
+    public class Token<T> : IToken<T>
+    {
+        public T type { get; }
+        public string value { get; }
+        public int position { get; }
+
+        public Token(T type, string value, int position)
+        {
+            this.type = type;
+            this.value = value;
+            this.position = position;
+        }
+    }
+
+    public class Token : IToken<string>
+    {
+        public string type { get; }
+        public string value { get; }
+        public int position { get; }
+
+        public Token(string type, string value, int position)
+        {
+            this.type = type;
+            this.value = value;
+            this.position = position;
+        }
+    }
+}
---
+++ Tokenizer/Tokenizer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PicTokenizer
+{
+    public sealed class Tokenizer<T>
+    {
+        private class TokenDefinition
+        {
+            public readonly T type;
+            private readonly Regex regex;
+
+            public TokenDefinition(T type, string regex)
+            {
+                this.type = type;
+                this.regex = new Regex(regex, RegexOptions.Compiled);
+            }
+
+            public MatchCollection GetMatches(string input) => regex.Matches(input);
+        }
+
+        private readonly List<TokenDefinition> tokenDefinitions = new List<TokenDefinition>();
+
+        public Tokenizer<T> WithToken(T type, params string[] regexes)
+        {
+            foreach (string regex in regexes)
+            {
+                tokenDefinitions.Add(new TokenDefinition(type, regex));
+            }
+            return this;
+        }
+
+        public IToken<T>[] Tokenize(string input)
+        {
+            bool[] occupied = new bool[input.Length];
+            List<IToken<T>> tokens = new List<IToken<T>>();
+
+            foreach (TokenDefinition definition in tokenDefinitions)
+            {
+                foreach (IToken<T> token in TokenizeInternal(input, occupied, definition))
+                {
+                    tokens.Add(token);
+                }
+            }
+
+            return tokens.OrderBy(t => t.position).ToArray();
+        }
+
+        private static IEnumerable<IToken<T>> TokenizeInternal(string input, bool[] occupied, TokenDefinition tokenDefinition)
+        {
+            foreach (Match match in tokenDefinition.GetMatches(input))
+            {
+                if (!match.Success)
+                    continue;
+
+                var indexRange = Enumerable.Range(match.Index, match.Length).ToList();
+                if (indexRange.Any(idx => occupied[idx]))
+                    continue;
+
+                indexRange.ForEach(idx => occupied[idx] = true);
+
+                yield return new Token<T>(tokenDefinition.type, match.Value, match.Index);
+            }
+        }
+    }
+}
